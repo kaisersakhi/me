bd798e9da4f83b03dce913cbbd06748334cd4bab
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-06-12 15:58:17 +0530

    Add: random background images

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,25 @@ import {
 import {FaBlog} from "react-icons/fa"
 import './styles.css'
 
-const img = "https://cdn.pixabay.com/photo/2018/04/19/08/22/background-3332557_1280.jpg"
+const img = [
+  "https://cdn.pixabay.com/photo/2018/04/19/08/22/background-3332557_1280.jpg",
+  "https://cdn.pixabay.com/photo/2023/05/17/08/55/tree-7999477_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg",
+  "https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_1280.jpg",
+  "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565_1280.jpg",
+  "https://cdn.pixabay.com/photo/2015/07/05/13/44/beach-832346_1280.jpg",
+  "https://cdn.pixabay.com/photo/2012/07/16/10/10/bridal-veil-fall-52450_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/03/02/16/54/iceland-2111810_1280.jpg"
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <section className='h-screen' style={{position: 'relative'}}>
-      <img src={img} alt="Image"  style={{ filter: 'blur(8px)' }} className='w-full h-full object-cover'/>
+      <img src={img[(Math.floor(Math.random() * img.length))]} alt="Image"  style={{ filter: 'blur(8px)' }} className='w-full h-full object-cover'/>
       <div className='flex items-center w-full h-full column' style={{ position: 'absolute', top: 0 }}>
-        <h1 className='bg-gradient-to-b from-gray-200  to-orange-200 text-transparent bg-clip-text p-10 text-center text-6xl w-full' style={{fontFamily: "BrittnaySignature"}}>Kaiser Sakhi</h1>
+        <h1 className='text-white p-10 text-center text-6xl w-full' style={{fontFamily: "BrittnaySignature"}}>Kaiser Sakhi</h1>
       </div>
 
 
